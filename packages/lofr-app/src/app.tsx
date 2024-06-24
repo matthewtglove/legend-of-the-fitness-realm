d9342ae81702f473f6b88e14bc076b8de9a0f262
Matthew Love <matthewtglove@gmail.com>
2024-06-23 21:41:36 -0500

    Load and select session

--- packages/lofr-app/src/app.tsx
+++ packages/lofr-app/src/app.tsx
@@ -1,9 +1,23 @@
-import { WorkoutLoader } from './workout/workout-loader';
+import { WorkoutProgram, WorkoutSession } from '@lofr/workout-parser';
+import { useState } from 'react';
+import { WorkoutLoader, WorkoutSelector } from './workout/workout-loader';
 
 export const App = () => {
+    const [workoutProgram, setWorkoutProgram] = useState(undefined as undefined | WorkoutProgram);
+    const [workoutSession, setWorkoutSession] = useState(undefined as undefined | WorkoutSession);
+
     return (
         <>
-            <WorkoutLoader />
+            <WorkoutLoader onWorkoutLoaded={setWorkoutProgram} />
+            {workoutProgram && (
+                <WorkoutSelector workoutProgram={workoutProgram} onWorkoutSessionSelected={setWorkoutSession} />
+            )}
+            {workoutSession && (
+                <div>
+                    <h1 className="m-6 text-2xl">Workout Session</h1>
+                    <pre className="m-6">{JSON.stringify(workoutSession, null, 2)}</pre>
+                </div>
+            )}
         </>
     );
 };
--- packages/lofr-app/src/workout/workout-loader.tsx
+++ packages/lofr-app/src/workout/workout-loader.tsx
@@ -1,28 +1,91 @@
-import { parseWorkoutDocument, WorkoutProgram } from '@lofr/workout-parser';
+import { parseWorkoutDocument, WorkoutProgram, WorkoutSession } from '@lofr/workout-parser';
 import { useState } from 'react';
 
-export const WorkoutLoader = () => {
+export const WorkoutLoader = ({ onWorkoutLoaded }: { onWorkoutLoaded: (workoutProgram: WorkoutProgram) => void }) => {
     const [workoutText, setWorkoutText] = useState(``);
     const [workoutProgram, setWorkoutProgram] = useState(undefined as undefined | WorkoutProgram);
     const loadWorkout = () => {
         const result = parseWorkoutDocument(workoutText);
         setWorkoutProgram(result);
+        onWorkoutLoaded(result);
     };
     return (
         <div>
             <h1 className="m-6 text-2xl">Workout Loader</h1>
             <textarea
-                className="p-2 m-6 w-full h-96 border-2"
-                placeholder="Paste your workout here"
+                className="w-full p-2 m-6 border-2 h-96"
+                placeholder="Paste your workout program here"
                 value={workoutText}
                 onChange={(e) => setWorkoutText(e.target.value)}
             />
-            <button className="p-2 ml-6 bg-blue-500 text-white" onClick={loadWorkout}>
-                Load Workout
+            <button className="p-2 m-6 text-white bg-blue-500" onClick={loadWorkout}>
+                Load Workout Program
             </button>
-            <div>
+            {/* <div>
                 <pre className="m-6">{JSON.stringify(workoutProgram, null, 2)}</pre>
+            </div> */}
+            <div>{workoutProgram?.name ?? ``}</div>
+        </div>
+    );
+};
+
+export const WorkoutSelector = ({
+    workoutProgram,
+    onWorkoutSessionSelected,
+}: {
+    workoutProgram: WorkoutProgram;
+    onWorkoutSessionSelected: (workoutSession: WorkoutSession) => void;
+}) => {
+    const [segmentNumber, setSegmentNumber] = useState(1);
+    const [sessionNumber, setSessionNumber] = useState(1);
+    const workoutSegment = workoutProgram.segments[segmentNumber - 1];
+    const workoutSession = workoutSegment?.sessions[sessionNumber - 1];
+
+    const loadSession = () => {
+        onWorkoutSessionSelected(workoutSession);
+    };
+
+    return (
+        <>
+            <h1 className="m-6 text-2xl">Workout Selector</h1>
+            <div>
+                <label className="m-6">Segment</label>
+                <input
+                    type="number"
+                    className="p-1 m-2 border-2"
+                    value={segmentNumber}
+                    onChange={(e) => setSegmentNumber(e.target.valueAsNumber)}
+                />
+                {workoutSegment?.name}
             </div>
+            <div>
+                <label className="m-6">Session</label>
+                <input
+                    type="number"
+                    className="p-1 m-2 border-2"
+                    value={sessionNumber}
+                    onChange={(e) => setSessionNumber(e.target.valueAsNumber)}
+                />
+                {workoutSession?.name}
+            </div>
+            <button className="p-2 m-6 text-white bg-blue-500" onClick={loadSession}>
+                Load Session
+            </button>
+            <ExpandableView title="Workout Session">
+                <pre className="m-6">{JSON.stringify(workoutSession, null, 2)}</pre>
+            </ExpandableView>
+        </>
+    );
+};
+
+const ExpandableView = ({ title, children }: { title: string; children: React.ReactNode }) => {
+    const [expanded, setExpanded] = useState(false);
+    return (
+        <div className="p-2 m-6 rounded bg-slate-200">
+            <button onClick={() => setExpanded(!expanded)}>
+                {expanded ? `⬇️` : `➡️`} {title}
+            </button>
+            {expanded && children}
         </div>
     );
 };
