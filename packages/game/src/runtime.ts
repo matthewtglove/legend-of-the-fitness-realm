7a06689e7015243f4f8205b1c651f427fe300aff
Rick Love <rick@toldpro.com>
2024-07-31 12:44:46 -0500

    wip implement game runtime

---
+++ packages/game/src/runtime.ts
@@ -0,0 +1,324 @@
+import {
+    CampaignContext,
+    GameBattleProvider,
+    GameCampaignId,
+    GameCharacter,
+    GameKeyItemId,
+    GameLocation,
+    GameLocationId,
+    GameLoreProvider,
+    GameQuestId,
+    GameRuntime,
+    GameState,
+} from './types';
+
+export const createGameRuntime = (
+    initialGameState: GameState,
+    lore: GameLoreProvider,
+    battle: GameBattleProvider,
+): GameRuntime => {
+    const state: GameState = JSON.parse(JSON.stringify(initialGameState));
+
+    const getSessionPlayers = ({ context }: { context: CampaignContext }) => {
+        return state.players.filter((x) => context.sessionPlayers.some((p) => p.player === x.id));
+    };
+    const getLocation = ({ context }: { context: CampaignContext }): GameLocation => {
+        const location = state.locations.find((x) => x.id === getSessionPlayers({ context })[0]?.location);
+        if (location) {
+            return location;
+        }
+
+        if (state.locations[0]) {
+            return state.locations[0];
+        }
+
+        // Create initial location
+        const { id, name } = lore.generateLocationInfo({
+            state,
+            campaignId: undefined,
+            questId: undefined,
+            nearbyLocationIds: [],
+        });
+        const newLocation = {
+            id,
+            name,
+            connections: [],
+        };
+        state.locations.push({ ...newLocation, connections: [] });
+        return newLocation;
+    };
+    const getLocationsNearby = ({
+        locationId,
+        distance,
+    }: {
+        locationId: GameLocationId;
+        distance: number;
+    }): GameLocation[] => {
+        const location = state.locations.find((x) => x.id === locationId);
+        if (!location) {
+            return [];
+        }
+        const connectedLocations = state.locations.filter((x) => location.connections.some((c) => c.location === x.id));
+        return [
+            location,
+            ...connectedLocations.flatMap((x) => getLocationsNearby({ locationId: x.id, distance: distance - 1 })),
+        ];
+    };
+    const addLocation = ({
+        location,
+        campaignId,
+        questId,
+        distance,
+    }: {
+        location: GameLocation;
+        campaignId?: GameCampaignId;
+        questId?: GameQuestId;
+        distance: number;
+    }) => {
+        const nearbyLocations = getLocationsNearby({ locationId: location.id, distance: 3 });
+        const { id, name } = lore.generateLocationInfo({
+            state,
+            campaignId: campaignId,
+            questId: questId,
+            nearbyLocationIds: nearbyLocations.map((x) => x.id),
+        });
+        const newLocation: GameLocation = {
+            id,
+            name,
+            connections: [{ location: location.id }],
+        };
+        location.connections.push({ location: newLocation.id });
+        state.locations.push(newLocation);
+
+        if (distance > 1) {
+            addLocation({ location: newLocation, campaignId, questId, distance: distance - 1 });
+        }
+    };
+
+    const ensureLocationsExits = ({
+        location,
+        distance,
+        isAcceptableLocation,
+        campaignId,
+        questId,
+    }: {
+        location: GameLocation;
+        distance: number;
+        isAcceptableLocation: (location: GameLocation) => boolean;
+        campaignId?: GameCampaignId;
+        questId?: GameQuestId;
+    }) => {
+        const nearbyLocations = getLocationsNearby({ locationId: location.id, distance: 3 });
+        const acceptableLocations = nearbyLocations.filter(isAcceptableLocation);
+        if (acceptableLocations.length) {
+            return { nearbyLocations, acceptableLocations };
+        }
+
+        // add locations
+        addLocation({
+            location,
+            campaignId,
+            questId,
+            distance,
+        });
+
+        return ensureLocationsExits({
+            location,
+            distance: distance,
+            isAcceptableLocation,
+            campaignId,
+            questId,
+        });
+    };
+
+    const getKeyItemsAtLocation = ({ locationId }: { locationId: GameLocationId }): GameKeyItemId[] => {
+        const location = state.locations.find((x) => x.id === locationId);
+        if (!location) {
+            return [];
+        }
+
+        return [
+            location.keyItem,
+            ...state.characters
+                .filter((x) => x.location === locationId && x.role.alignment === `enemy`)
+                .map((x) => x.keyItem),
+        ]
+            .filter((x) => x)
+            .map((x) => x!);
+    };
+
+    const getPathToLocation = ({
+        startLocationId,
+        endLocationId,
+        obtainedKeyItemIds: obtainedKeyItemIds_init,
+    }: {
+        startLocationId: GameLocationId;
+        endLocationId: GameLocationId;
+        obtainedKeyItemIds: GameKeyItemId[];
+    }): undefined | GameLocationId[] => {
+        const startLocation = state.locations.find((x) => x.id === startLocationId);
+        if (!startLocation) {
+            return undefined;
+        }
+
+        const obtainedKeyItemIdsSet = new Set(obtainedKeyItemIds_init);
+        const visitedLocations = new Set<GameLocationId>();
+        const queue = [{ location: startLocation, path: [] as GameLocationId[] }];
+        while (queue.length) {
+            const { location, path } = queue.shift()!;
+            if (location.id === endLocationId) {
+                return path;
+            }
+
+            visitedLocations.add(location.id);
+
+            const keyItems = getKeyItemsAtLocation({ locationId: location.id });
+            keyItems.forEach((keyItem) => obtainedKeyItemIdsSet.add(keyItem));
+
+            for (const connection of location.connections) {
+                if (connection.requiredKeyItem && !obtainedKeyItemIdsSet.has(connection.requiredKeyItem)) {
+                    continue;
+                }
+
+                const connectedLocation = state.locations.find((x) => x.id === connection.location);
+                if (!connectedLocation || visitedLocations.has(connectedLocation.id)) {
+                    continue;
+                }
+
+                queue.push({ location: connectedLocation, path: [...path, connectedLocation.id] });
+            }
+        }
+
+        return undefined;
+    };
+
+    const ensureCampaignExists = ({ context }: { context: CampaignContext }) => {
+        const currentCampaign = state.campaigns.find((x) => !x.isComplete);
+        if (currentCampaign) {
+            return currentCampaign;
+        }
+
+        const { id, name } = lore.generateCampaignInfo({
+            state,
+            playerLevels: getSessionPlayers({ context }).map((x) => x.stats.level),
+            locationId: getLocation({ context })?.id,
+            previousCampaignIds: state.campaigns.map((x) => x.id),
+        });
+        const newCampaign = {
+            id,
+            name,
+            quests: [],
+        };
+        state.campaigns.push(newCampaign);
+        return newCampaign;
+    };
+
+    const ensureQuestExists = ({ context }: { context: CampaignContext }) => {
+        const nextIncompleteQuest = state.quests.find((x) => !x.isComplete);
+        if (nextIncompleteQuest) {
+            return nextIncompleteQuest;
+        }
+
+        const campaign = ensureCampaignExists({ context });
+        const newQuestInfo = lore.generateQuestInfo({
+            state,
+            playerLevels: getSessionPlayers({ context }).map((x) => x.stats.level),
+            locationId: getLocation({ context }).id,
+            campaignId: campaign?.id,
+            previousQuestIds: state.quests.map((x) => x.id),
+        });
+        const newQuest = {
+            id: newQuestInfo.id,
+            name: newQuestInfo.name,
+            objectives: newQuestInfo.objectives.map((x) => ({
+                name: x.name,
+                completionKeyItem: x.completionKeyItem.id,
+            })),
+        };
+        state.quests.push(newQuest);
+
+        const keyItems = newQuestInfo.objectives.map((x) => x.completionKeyItem);
+        state.keyItems.push(...keyItems);
+
+        // Place key items in a location or on a enemy character
+
+        const location = getLocation({ context });
+        const reachedLocationIdsSet = new Set(state.locations.filter((x) => x.isDiscovered).map((x) => x.id));
+        const obtainedKeyItemIdsSet = new Set(state.keyItems.filter((x) => x.isObtained).map((x) => x.id));
+
+        for (const keyItem of keyItems) {
+            // place key item in a nearby location (or enemy in location)
+
+            // choose unreached nearby location
+            const { acceptableLocations: unreachedLocations } = ensureLocationsExits({
+                location,
+                distance:
+                    context.campaignSessionsRemaining <= 0 ? Math.min(3, context.sessionPeriodsRemaining.length) : 3,
+                isAcceptableLocation: (x) =>
+                    !reachedLocationIdsSet.has(x.id) && (keyItem.placement !== `location` || !x.keyItem),
+                campaignId: campaign.id,
+                questId: newQuest.id,
+            });
+
+            // Randomly select location
+            const unreachedLocation = unreachedLocations[Math.floor(Math.random() * unreachedLocations.length)]!;
+
+            // Update obtained state for path to location
+            const pathToLocation = getPathToLocation({
+                startLocationId: location.id,
+                endLocationId: unreachedLocation.id,
+                obtainedKeyItemIds: [...obtainedKeyItemIdsSet],
+            })!;
+            pathToLocation.forEach((locationId) => {
+                reachedLocationIdsSet.add(locationId);
+                const keyItems = getKeyItemsAtLocation({ locationId });
+                keyItems.forEach((keyItem) => obtainedKeyItemIdsSet.add(keyItem));
+            });
+
+            if (keyItem.placement === `location`) {
+                // place key item at location
+                unreachedLocation.keyItem = keyItem.id;
+                obtainedKeyItemIdsSet.add(keyItem.id);
+                continue;
+            }
+
+            // add new enemy at location holding the key item
+            const playerLevels = getSessionPlayers({ context }).map((x) => x.stats.level);
+            const newEnemyInfo = lore.generateEnemyInfo({
+                state,
+                playerLevels,
+                locationId: unreachedLocation.id,
+                enemyDifficulty: `normal`,
+            });
+            const newEnemyStats = battle.generateEnemyStats({
+                state,
+                playerLevels,
+                enemyDifficulty: newEnemyInfo.role.enemyDifficulty,
+            });
+            const newEnemy: GameCharacter = {
+                id: newEnemyInfo.id,
+                name: newEnemyInfo.name,
+                role: { ...newEnemyInfo.role, alignment: `enemy` },
+                stats: { ...newEnemyStats.stats },
+                equipment: {},
+                inventory: [],
+                location: unreachedLocation.id,
+                keyItem: keyItem.id,
+            };
+            state.characters.push(newEnemy);
+        }
+
+        return newQuest;
+    };
+
+    return {
+        state,
+        triggerSessionStart: ({ context }) => {
+            ensureQuestExists({ context });
+
+            // TODO: Communicate quest to players
+
+            return [];
+        },
+    };
+};
--- packages/game/src/types.ts
+++ packages/game/src/types.ts
@@ -1,33 +1,129 @@
+// MARK: GameState
 export type GameState = {
     players: GameCharacter[];
     locations: GameLocation[];
     characters: GameCharacter[];
     keyItems: GameKeyItem[];
     items: GameItem[];
+    campaigns: GameCampaign[];
+    quests: GameQuest[];
 };
 
+// MARK: GameLoreProvider
+export type GameLoreProvider = {
+    generateEnemyInfo(props: {
+        state: GameState;
+        playerLevels: number[];
+        locationId: GameLocationId;
+        enemyDifficulty: GameEnemyDifficulty;
+    }): {
+        id: GameCharacterId;
+        name: string;
+        role: {
+            race: string;
+            class: string;
+            enemyDifficulty: GameEnemyDifficulty;
+        };
+    };
+    generateCampaignInfo: (props: {
+        state: GameState;
+        playerLevels: number[];
+        locationId?: GameLocationId;
+        previousCampaignIds: GameCampaignId[];
+    }) => { id: GameCampaignId; name: string };
+    generateQuestInfo: (props: {
+        state: GameState;
+        playerLevels: number[];
+        locationId?: GameLocationId;
+        campaignId?: GameCampaignId;
+        previousQuestIds: GameQuestId[];
+    }) => {
+        id: GameQuestId;
+        name: string;
+        objectives: {
+            name: string;
+            completionKeyItem: {
+                id: GameKeyItemId;
+                name: string;
+                isVisible: boolean;
+                placement: `enemy` | `location`;
+            };
+        }[];
+    };
+    generateLocationInfo: (props: {
+        state: GameState;
+        campaignId?: GameCampaignId;
+        questId?: GameQuestId;
+        nearbyLocationIds: GameLocationId[];
+    }) => {
+        id: GameLocationId;
+        name: string;
+    };
+};
+
+// MARK: GameBattleProvider
+export type GameBattleProvider = {
+    generateEnemyStats: (props: { state: GameState; playerLevels: number[]; enemyDifficulty: GameEnemyDifficulty }) => {
+        stats: GameCharacter[`stats`];
+    };
+};
+
+// MARK: GameCampaign
+export type GameCampaignId = string & { __type: `GameCampaignId` };
+export type GameCampaign = {
+    id: GameCampaignId;
+    name: string;
+    quests: GameQuestId[];
+    isComplete?: boolean;
+};
+
+// MARK: GameQuest
+export type GameQuestId = string & { __type: `GameQuestId` };
+export type GameQuest = {
+    id: GameQuestId;
+    name: string;
+    objectives: GameQuestObjective[];
+    isComplete?: boolean;
+};
+
+export type GameQuestObjective = {
+    name: string;
+    completionKeyItem: GameKeyItemId;
+};
+
+// MARK: GameLocation
 export type GameLocationId = string & { __type: `GameLocationId` };
 export type GameLocation = {
     id: GameLocationId;
     name: string;
-    connections: GameLocationId[];
-    /** connected rooms are blocked until the key item is obtained */
-    requiredKeyItem?: GameKeyItemId;
+    connections: {
+        location: GameLocationId;
+        name?: string;
+        isDiscovered?: boolean;
+        /** connected rooms are blocked until the key item is obtained */
+        requiredKeyItem?: GameKeyItemId;
+    }[];
+    /** have the players reached this location yet */
+    isDiscovered?: boolean;
     /** rooms can have a key item that can be discovered by searching the room */
     keyItem?: GameKeyItemId;
 };
 
+// MARK: GameCharacter
 export type GameCharacterId = string & { __type: `GameCharacterId` };
 export type GameCharacter = {
     id: GameCharacterId;
     name: string;
     location: GameLocationId;
+    /** Have the players encountered this character yet */
+    isDiscovered?: boolean;
     /** characters can hold a key item that can be obtained after defeating evil enemy or by earning via a friendly interaction: puzzle, trade, etc */
     keyItem?: GameKeyItemId;
     role: {
         race: string;
         class: string;
-        alignment: `good` | `evil`;
+        alignment: `friend` | `enemy`;
+        enemyDifficulty?: GameEnemyDifficulty;
     };
     stats: {
         level: number;
@@ -43,8 +139,8 @@ export type GameCharacter = {
         intelligence: number;
     };
     equipment: {
-        weapon: GameItemId;
-        armor: GameItemId;
+        weapon?: GameItemId;
+        armor?: GameItemId;
     };
     inventory: {
         item: GameItemId;
@@ -52,13 +148,18 @@ export type GameCharacter = {
     }[];
 };
 
+export type GameEnemyDifficulty = `normal` | `minor-boss` | `major-boss` | `final-boss`;
+
+// MARK: GameKeyItem
 export type GameKeyItemId = string & { __type: `GameKeyItemId` };
 export type GameKeyItem = {
     id: GameKeyItemId;
     name: string;
-    obtained: boolean;
+    isVisible: boolean;
+    isObtained?: boolean;
 };
 
+// MARK: GameItem
 export type GameItemId = string & { __type: `GameItemId` };
 export type GameItem = {
     id: GameItemId;
@@ -71,6 +172,7 @@ export type GameItem = {
     };
 };
 
+// MARK: GameRuntime
 export type GameRuntime = {
     state: GameState;
 
@@ -79,28 +181,80 @@ export type GameRuntime = {
     /** Provide cliffhanger for next session */
     triggerSessionEnd: (options: { context: CampaignContext }) => GameEventResponse;
     /** For long work periods provide periodic game events */
-    triggerWorkPeriod: (options: { durationSec: number; context: CampaignContext }) => GameEventResponse;
+    triggerWorkPeriod: (options: { context: CampaignContext }) => GameEventResponse;
     /** Provide game event and for long rest periods offer player decision */
     triggerRestPeriod: (options: {
-        durationSec: number;
-        successRatio: number;
         context: CampaignContext;
+
+        /** This assumes work results are entered immediately, but it might take time:
+         *
+         * - [yes] Should this only be called after all local players enter their results?
+         *   - Yes, the game can't continue without the results (or assuming success)
+         *   - The UI can give a short time to allow for entering results and then call this after a timeout with assumed success
+         * - [no] Should this be called immediately and have a short response while players enter their results?
+         *   - [no] Is the game logic able to do anything without the results?
+         */
+        workResults: PlayerWorkResult[];
     }) => GameEventResponse;
+
+    /** Add new player */
+    createPlayer: (options: {
+        characterName: string;
+        characterRole: string;
+        characterClass: string;
+    }) => GameEventResponse;
+
     /** Handle player decision */
     enterPlayerDecision: (options: { decisionId: GameDecisionId; context: CampaignContext }) => GameEventResponse;
+
+    /** Allow remote player success data to collect asynchonously, this will likely be silent but used later in future events
+     *
+     * This can happen at any time as the session timing is different for a remote player:
+     *
+     * Remote players may be following a different workout program and have different work periods, etc.
+     */
+    enterRemotePlayersWorkResult: (options: {
+        remoteSessionPeriods: { kind: `work` | `rest`; durationSec: number }[];
+        workResults: PlayerWorkResult[];
+    }) => GameEventResponse;
 };
 
-type CampaignContext = {
+type PlayerWorkResult = {
+    player: GameCharacterId;
+    sessionPeriodIndex: number;
+    /** The player's success level for the work period:
+     *
+     * Is this measured or self-reported?
+     *
+     * - weak: the player feels weak (bad attitude: give up, not trying, tired, complaining, etc)
+     * - normal: the player feels normal (good attitude: ok, good, fine)
+     * - strong: the player feels strong (great attitude: great, excellent, amazing)
+     */
+    successKind: `weak` | `normal` | `strong`;
+};
+
+// MARK: CampaignContext
+export type CampaignContext = {
+    sessionPlayers: {
+        player: GameCharacterId;
+        /** Local players should report success at beginning of rest period */
+        isLocal: boolean;
+    }[];
+    currentSessionPeriod: {
+        index: number;
+        remainingSec: number;
+    };
+    sessionPeriods: { kind: `work` | `rest`; durationSec: number }[];
     sessionPeriodsRemaining: { kind: `work` | `rest`; durationSec: number }[];
-    sessionPeriodsTotal: { kind: `work` | `rest`; durationSec: number }[];
-    campaignSessionsRemaining: number;
     campaignSessionsTotal: number;
+    campaignSessionsRemaining: number;
 };
 
 export type GameEventResponse = {
     events: GameEvent[];
 };
 
+// MARK: GameEvent
 /** A game event is something that should be communicated to the player
  *
  * It can be read directly as a message or used to prompt an AI message
