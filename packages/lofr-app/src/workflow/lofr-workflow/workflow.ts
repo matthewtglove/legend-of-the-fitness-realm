d4888f26864d462063f5f6f269c1e667f4819fab
Rick Love <rick@toldpro.com>
2025-12-17 10:39:28 -0600

    wip move to doc

--- packages/lofr-app/src/workflow/lofr-workflow/workflow.ts
+++ packages/lofr-app/src/workflow/lofr-workflow/workflow.ts
@@ -101,11 +101,11 @@ export const loadLofrWorkflow = async (workflowEditorController: WorkflowEditorC
   // });
 
 
+  /*
   workflowEditorController.addTextFileNode({ id: `n-workflow`, path: `workflow/lofr-workflow/workflow.ts` });
   workflowEditorController.addTextFileNode({ id: `n-workflow-metadata`, path: `workflow/lofr-workflow/workflow.metadata.json` });
 
   workflowEditorController.addTextFileNode({ id: `n-example-fun`, path: `workflow/lofr-workflow/example-fun.ts` });
-  workflowEditorController.addTextNode({ id: `n-example-fun-result`, content: exampleFun() });
 
   workflowEditorController.addTextFileNode({ id: `n-clock-mini-game-code`, path: `prep/clock-mini-game/game-view.tsx` });
   workflowEditorController.addComponent({
@@ -132,6 +132,9 @@ export const loadLofrWorkflow = async (workflowEditorController: WorkflowEditorC
     },
     text: `hello!!!\nhello!!!\nhello!!!\nhello!!!\nhello!!!\nhello!!!\n`
   });
+  */
+
+  workflowEditorController.addTextNode({ id: `n-example-fun-result`, content: exampleFun() });
 
   const todoNode = workflowEditorController.addTextFileNode({ id: `n-todo`, path: `workflow/todo.md` });
   workflowEditorController.addComponent({
